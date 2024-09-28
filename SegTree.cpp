5cc5f04d66e002ca91c2b9c428ddefa3d4384ef4
Frank-Steven <FrankStevenTool@outlook.com>
2024-09-28 10:38:46 +0800

    new file:   SegTree.cpp

---
+++ SegTree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename Seg, typename Tag>
+struct SegTree {
+    Seg seg; Tag tag = Tag();
+    int l, r, mid;
+    SegTree *ls = NULL, *rs = NULL;
+    template <typename Func>
+    SegTree(int s, int e, const Func &init):
+      l(s), r(e), mid(l+r>>1) {
+        if(l == r) { seg = init(l); return; }
+        ls = new SegTree(l,mid,init);
+        rs = new SegTree(mid+1,r,init);
+        seg = Seg::merge(ls->seg,rs->seg);
+    }
+    void release() {
+        if(ls) ls->recieve(tag);
+        if(rs) rs->recieve(tag);
+        tag = Tag();
+    }
+    void recieve(const Tag &x) {
+        seg.apply(x);
+        tag = Tag::merge(tag,x);
+    }
+    void modify(int s, int e, const tag &x) {
+        if(s <= l && r <= e) { recieve(x); return; }
+        release();
+        if(s <= mid) ls->modify(s,e,x);
+        if(e >  mid) rs->modify(s,e,x);
+        seg = Seg::merge(ls->seg,rs->seg);
+    }
+    Seg query(int s, int e) {
+        if(s <= l && r <= e) return seg;
+        release();
+        if(e <= mid) return ls->query(s,e);
+        if(s >  mid) return rs->query(s,e);
+        return Seg::merge(ls->query(s,e),rs->query(s,e));
+    }
+};
+int main() {
+    return 0;
+}
