408c6632df361a6d9ca706851682f99b4b9a27f1
Frank-Steven <FrankStevenTool@outlook.com>
2024-09-29 15:03:59 +0800

    modified:   ForInRange.cpp

--- ForInRange.cpp
+++ ForInRange.cpp
@@ -3,22 +3,22 @@ using namespace std;
 template <typename _Ty, typename Func>
 constexpr void for_r(_Ty s, _Ty e, const Func &f) {
     _Ty i;
-    if (s < e) {
-        for (i = s; i <= e - 4; i += 4) {
+    if(s < e) {
+        for(i = s; i <= e - 4; i += 4) {
             f(i);
             f(i + 1);
             f(i + 2);
             f(i + 3);
         }
-        for (; i <= e; ++i) f(i);
+        for(; i <= e; ++i) f(i);
     } else {
-        for (i = s; i >= e + 4; i -= 4) {
+        for(i = s; i >= e + 4; i -= 4) {
             f(i);
             f(i - 1);
             f(i - 2);
             f(i - 3);
         }
-        for (; i >= e; --i) f(i);
+        for(; i >= e; --i) f(i);
     }
 }
 int main() {
